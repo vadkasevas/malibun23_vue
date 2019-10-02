72618f01aa7c5332f00d39114c03752dbb64c4ce
vadkasevas <v.tikkoyev@mail.ru>
2019-10-02 19:30:16 +0300

    group visible

--- lib/vue-form-generator/formGenerator.vue
+++ lib/vue-form-generator/formGenerator.vue
@@ -3,13 +3,15 @@ div.vue-form-generator(v-if='schema != null && subscriptionsReady')
 
 	fieldset(v-if="schema.fields", :is='tag')
 		template(v-for='field in fields')
-			form-group(v-if='fieldVisible(field)', :vfg="vfg", :field="field", :errors="errors", :model="model", :options="options", @validated="onFieldValidated", @model-updated="onModelUpdated")
+			form-group(v-if='fieldVisible(field)', :vfg="vfg", :field="field", :errors="errors", :model="model", :options="options",
+                @validated="onFieldValidated", @model-updated="onModelUpdated")
 
 	template(v-for='group in groups')
-		fieldset(:is='tag', :class='getFieldRowClasses(group)')
+        fieldset(v-if='fieldVisible(group)', :is='tag', :class='getFieldRowClasses(group)')
 			legend(v-if='group.legend') {{ group.legend }}
 			template(v-for='field in group.fields')
-				form-group(v-if='fieldVisible(field)', :vfg="vfg", :field="field", :errors="errors", :model="model", :options="options", @validated="onFieldValidated", @model-updated="onModelUpdated")
+				form-group(v-if='fieldVisible(field)', :vfg="vfg", :field="field", :errors="errors", :model="model", :options="options",
+                    @validated="onFieldValidated", @model-updated="onModelUpdated")
 
 vue-loading(v-else)
 </template>
