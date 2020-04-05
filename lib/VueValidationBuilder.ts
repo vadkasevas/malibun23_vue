59bf6dce7a2e4e7691f5f1629febba1a4e070f52
vadkasevas <vadkasevas@mail.ru>
2020-04-05 08:04:57 +0300

    disks

--- lib/VueValidationBuilder.ts
+++ lib/VueValidationBuilder.ts
@@ -1,5 +1,5 @@
 ///<reference path="../typings/meteor/malibun23:stack.d.ts"/>
-import {MalibunCollection} from "meteor/malibun23:stack";
+import {isset, MalibunCollection, safeGet} from "meteor/malibun23:stack";
 //@ts-ignore
 import {_} from "meteor/underscore";
 //@ts-ignore
@@ -75,6 +75,72 @@ export class VueValidationBuilder {
             });
         });
         return this;
+    }
+
+    static namedAutoComplete(collection,fieldName,schemaOptions,options):any{
+        let multiple = safeGet(options,'multiple',false);
+        fieldName = fieldName || 'name';
+        let collectionName = _.isString(collection)? collection:collection._name;
+
+        var getCollection = function(){
+            //@ts-ignore
+            return MalibunCollection.byName(collectionName);
+        };
+        var methodName = `${collectionName}namedAutoComplete`;
+        if(safeGet(options,'multiple',false)){
+            methodName+='Multiple';
+        }
+        var limit = safeGet(options,'limit',0);
+        if(limit){
+            methodName+=`Limit${limit}`;
+        }
+        var methodFunction = null;
+        if(_.isString(safeGet(options,'method',methodName) )){
+            methodName = safeGet(options,'method',methodName);
+        }else{
+            methodFunction = safeGet(options,'method',null);
+        }
+        methodName = safeGet(options,'method',methodName);
+
+        var transform = safeGet(options,'transform',function (model) {
+            return {label: safeGet( model,fieldName,''), value: model._id};
+        });
+
+        if(Meteor.isServer){
+            //@ts-ignore
+            if(!isset(Meteor.default_server.method_handlers[methodName])) {
+                var method = {};
+                method[methodName] = methodFunction || function (options) {
+                    let collection = getCollection();
+                    this.unblock();
+                    var findOptions = limit ? {limit:limit} : {};
+                    var condition = options.searchText ? {$or : [{ [fieldName]: {$regex:new RegExp(options.searchText, "i") }} ] } : {};
+                    //console.log(condition);
+                    if(options.values&&options.values.length>0) {
+                        condition.$or = condition.$or || [];
+                        //@ts-ignore
+                        condition.$or.push( { _id:{ $in: options.values} } );
+                        if(limit)
+                            findOptions = {limit:limit+options.values.length};
+                    }
+
+                    //@ts-ignore
+                    var cursor = (collection instanceof MalibunCollection && !Meteor.isAdmin() ) ?  collection.publishCursor( Meteor.currentUser(), condition, options )
+                        :collection.find(condition,findOptions);
+
+                    return cursor.fetch().map(function (model) {
+                        return transform(model);
+                    });
+                };
+                Meteor.methods(method);
+            }
+        }
+
+
+        schemaOptions['type'] = 'autocomplete';
+        schemaOptions['multiple'] = !!multiple;
+        schemaOptions['methodName'] = methodName;
+        return schemaOptions;
     }
 
     userId():this{
